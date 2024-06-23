d7d6fa9d7f2ff033473afbd79cd544e537f8bbee
Riteshk1314 <riteshkapoor1314@gmail.com>
2024-06-23 18:31:36 +0530

    autho config

--- backend/studentPannel/models.py
+++ backend/studentPannel/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.contrib.postgres.fields import ArrayField
 
-# Create your models here.
+class User(models.Model):
+    image = models.URLField()
+    registration_id = models.CharField(unique=True, max_length=20)
+    questions_attempted=models.JSONField(null=True, blank=True)
+    score= models.IntegerField(default=0)
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+    
